914b0063c802b927c404f8b121dba08ce20ae0d1
JacksonBair <Jacksonbair@gmail.com>
2023-08-11 21:28:18 -0500

    Created settings and changed menus
    Created the basis for audio settings. Created the options menu.
    Created main menu.
    Created global settings script that runs on start.

---
+++ src/scenes/menus/main_manu/Main.gd
@@ -0,0 +1,18 @@
+extends Control
+
+onready var options_menu = $OptionsMenu
+
+func _on_start_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_options_button_pressed():
+	options_menu.popup_centered()
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
+
+
+func _on_credits_button_pressed():
+	pass # Replace with function body.
