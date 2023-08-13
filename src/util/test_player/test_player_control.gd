7d4cd7274a4a77f4de4ec79a19fa76ae3b5ce76a
MahmoudN77 <mt.nasereddin@gmail.com>
2023-08-12 20:42:59 -0500

    add Coyote jump to the player

--- src/util/test_player/test_player_control.gd
+++ src/util/test_player/test_player_control.gd
@@ -6,6 +6,10 @@ var gravity : int = 200
 var gravity_state = true
 var velocity = Vector2()
 
+onready var Animated_Sprite = $AnimatedSprite
+onready var coyote_jump_timer  = $CoyoteJumpTimer
+
+
 func get_input(delta):
 	velocity.x = 0
 	if Input.is_action_pressed('move_right'):
@@ -13,15 +17,20 @@ func get_input(delta):
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= speed
 	if Input.is_action_just_pressed("jump"):
-		if (is_on_floor()):
+		if (is_on_floor()) or coyote_jump_timer.time_left > 0.0:
 			velocity.y += jump_speed
-		if (is_on_ceiling()):
-			velocity.y -= jump_speed
+		if not is_on_floor():
+			if (is_on_ceiling()):
+				velocity.y -= jump_speed
 	if Input.is_action_just_pressed("grav_shift"):
 			touching_floor()
 			
 	velocity.y += gravity * delta
+	var was_on_floor = is_on_floor()
 	velocity = move_and_slide(velocity, Vector2.UP)
+	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0
+	if just_left_ledge:
+		coyote_jump_timer.start()
 	
 func grav_shift():
 	if gravity_state == false:
