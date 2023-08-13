29aeea22b6efbb895b1254d5f50ed4200cf01f40
MahmoudN77 <mt.nasereddin@gmail.com>
2023-08-12 22:28:22 -0500

    add double jump for player

--- src/util/test_player/test_player_control.gd
+++ src/util/test_player/test_player_control.gd
@@ -5,6 +5,7 @@ var jump_speed : int = -200
 var gravity : int = 200
 var gravity_state = true
 var velocity = Vector2()
+var air_jump = false
 
 onready var Animated_Sprite = $AnimatedSprite
 onready var coyote_jump_timer  = $CoyoteJumpTimer
@@ -17,21 +18,29 @@ func get_input(delta):
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= speed
 	if Input.is_action_just_pressed("jump"):
+		if is_on_floor(): air_jump = true
+		
 		if (is_on_floor()) or coyote_jump_timer.time_left > 0.0:
 			velocity.y += jump_speed
+			
 		if not is_on_floor():
+			if Input.is_action_just_pressed("jump") and air_jump:
+				velocity.y += jump_speed * 0.8
+				air_jump = false
+				
 			if (is_on_ceiling()):
 				velocity.y -= jump_speed
 	if Input.is_action_just_pressed("grav_shift"):
 			touching_floor()
 			
 	velocity.y += gravity * delta
+
 	var was_on_floor = is_on_floor()
 	velocity = move_and_slide(velocity, Vector2.UP)
 	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0
 	if just_left_ledge:
 		coyote_jump_timer.start()
-	
+			
 func grav_shift():
 	if gravity_state == false:
 		gravity += 400
