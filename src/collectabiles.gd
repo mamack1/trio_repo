5572bc735c09f8c74dad9ce03845fcf8984f1721
MahmoudN77 <mt.nasereddin@gmail.com>
2023-08-17 12:01:23 -0500

    create Gems, Key, ...

--- src/collectabiles.gd
+++ src/collectabiles.gd
@@ -1,6 +1,8 @@
 extends "res://scripts/player/HUD.gd"
 
-func _on_Key_body_entered(body):
+
+func _on_Coin_body_entered(body):
 	if body.name == "KinematicBody2D":
+		get_node("/root/Hud").score += 5
 		queue_free()
 	pass # Replace with function body.
