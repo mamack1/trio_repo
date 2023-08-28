8f29ef76eed027241b0d49956720f5f468259ad9
mamack1 <122715484+mamack1@users.noreply.github.com>
2023-08-28 13:55:08 -0500

    counter fix
    fixed end scene coin counter issue

--- src/scripts/collectibles/coin_new.gd
+++ src/scripts/collectibles/coin_new.gd
@@ -4,12 +4,13 @@ signal coin_collected
 
 func _on_Gems_body_entered(body):
 	if body.name == "KinematicBody2D":
+		set_collision_mask_bit(0, false)
+		set_collision_layer_bit(0, false)
 		#get_node("/root/Hud").score += 5
 		$collect_sound.play()
 		$AnimationPlayer.play("bounce")
 		emit_signal("coin_collected")
-		set_collision_mask_bit(0, false)
-		set_collision_layer_bit(0, false)
+
 		#yield(get_tree().create_timer(0.2), "timeout")
 		
 func _on_AnimationPlayer_animation_finished(anim_name):
